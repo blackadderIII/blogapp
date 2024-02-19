65137579efba284cddc2f83f9f982ece49521044
blackadderIII <skobarnes1@gmail.com>
2024-02-19 20:51:53 +0000

    ..

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Welcome To New Genesis.
+          
         </p>
         <a
           className="App-link"
