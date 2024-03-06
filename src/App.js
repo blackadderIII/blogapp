00448df4fa95f7b41fe505c4d24f19d70cd4b4ad
blackadderIII <skobarnes1@gmail.com>
2024-03-06 20:21:34 +0000

    early stage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-
-import './App.css';
-import './all.css';
-import './navtoggle.js';
+import "./App.css";
+import "./all.css";
+import Navbar from './navbar';
+import Home from "./home";
 
 function App() {
-const title ='Welcome To New Genesis';
-const likes = 50;
-// const person ={ name:'yoshi',age:'50'};
+  const title = "Welcome To New Genesis";
+  const likes = 50;
+  const link = 'http://www.google.com'
+  // const person ={ name:'yoshi',age:'50'};
 
   return (
     <div className="App">
+    <Navbar/>
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        {/* <p>{person}</p> */}
+        <Home/>
       </div>
-        {/* <div className="nav active" id='nav'>
+      {/* <div className="nav active" id='nav'>
           <a href="1"><i className="fa-game-console-handheld"></i><h4>PS4</h4></a>
           <a href="1"><i className="fa fa-galaxy"></i><h4>XBOX</h4></a>
           <a href="1"><i className="fa-game-console-handheld"></i><h4>PS5</h4></a>
---
+++ src/navbar.js
@@ -0,0 +1,14 @@
+function Navbar(){
+   return(
+    <nav className="navbar">
+        <h1>The Saiyan Blog </h1>
+        <div className="links">
+            <a href="/">Home</a>
+            <a href="/create">New Blog</a>
+        </div>
+    </nav>
+   );
+    
+}
+
+export default Navbar
