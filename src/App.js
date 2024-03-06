272a1da2a2360b32be619b8c3e529e77a59021a2
blackadderIII <skobarnes1@gmail.com>
2024-03-06 19:41:58 +0000

    .

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 import './all.css';
 import './navtoggle.js';
 
 function App() {
+const title ='Welcome To New Genesis';
+const likes = 50;
+// const person ={ name:'yoshi',age:'50'};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome To New Genesis.
-          <br/>  CHAPTER 1
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        {/* <p>{person}</p> */}
+      </div>
         {/* <div className="nav active" id='nav'>
           <a href="1"><i className="fa-game-console-handheld"></i><h4>PS4</h4></a>
           <a href="1"><i className="fa fa-galaxy"></i><h4>XBOX</h4></a>
