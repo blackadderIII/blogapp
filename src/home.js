1b5ea8813aa01cbb99fce162fb0e425d63f99da2
blackadderIII <skobarnes1@gmail.com>
2024-03-07 10:59:53 +0000

    .

--- src/home.js
+++ src/home.js
@@ -5,17 +5,19 @@ const Home = () => {
 
     // let name = 'SKOB'
      const[name,setName] = useState('SKOB')
+     const[age, setage]= useState('35')
 
     const handleClick = ()=>{
-       setName('Goku')
-       console.log(name)
+       setName('Goku') 
+        setage(50)
+
     }
 
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
-            <p>{name}</p>
+            <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click Me</button>
         </div>
     );
--- src/navbar.js
+++ src/navbar.js
@@ -3,7 +3,11 @@ function Navbar(){
     <nav className="navbar">
         <h1>The Saiyan Blog </h1>
         <div className="links">
-            <a href="/">Home</a>
+            <a href="/" style={{
+                color:'white',
+                backgroundColor:'#f1356d',
+                borderRadius: '8px'
+            }}>Home</a>
             <a href="/create">New Blog</a>
         </div>
     </nav>
