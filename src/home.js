e4f267b0a18018e346b9f2cf0ef0cae8425ba814
blackadderIII <skobarnes1@gmail.com>
2024-03-07 12:51:29 +0000

    .

--- src/home.js
+++ src/home.js
@@ -1,26 +1,23 @@
-import {useState} from 'react'
-
+import { useState } from "react";
+import Bloglist from "./bloglist";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "SSJ1 - Genesis", body: "lorem ipsum...", author: "John", id: 1 },
+    { title: "SSJ2 - Exodus", body: "lorem ipsum...", author: "Samuel", id: 2 },
+    { title: "SSJ3 - Kings", body: "lorem ipsum...", author: "Barnes", id: 3 },
+    {
+      title: "SSJ4 - Revelations",
+      body: "lorem ipsum...",
+      author: "SKOB",
+      id: 4,
+    },
+  ]);
+  return (
+  <div className="home">
+   <Bloglist  blogs={blogs}/>
+  </div>
+  );
+};
 
-    // let name = 'SKOB'
-     const[name,setName] = useState('SKOB')
-     const[age, setage]= useState('35')
-
-    const handleClick = ()=>{
-       setName('Goku') 
-        setage(50)
-
-    }
-
-
-    return ( 
-        <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>Click Me</button>
-        </div>
-    );
-}
- 
 export default Home;
