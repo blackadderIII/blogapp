7791715670c41e8b7d245c2e48e75227e7bcc0aa
blackadderIII <skobarnes1@gmail.com>
2024-03-07 10:26:58 +0000

    day 2.

--- src/home.js
+++ src/home.js
@@ -1,7 +1,22 @@
+import {useState} from 'react'
+
+
 const Home = () => {
+
+    // let name = 'SKOB'
+     const[name,setName] = useState('SKOB')
+
+    const handleClick = ()=>{
+       setName('Goku')
+       console.log(name)
+    }
+
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name}</p>
+            <button onClick={handleClick}>Click Me</button>
         </div>
     );
 }
