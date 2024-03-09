214c95c13041506f1557e71f6535d4213a7850cd
blackadderIII <skobarnes1@gmail.com>
2024-03-09 22:02:30 +0000

    ..

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-
 import "./all.css";
 import Navbar from './navbar';
 import Home from "./home";
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+import CreateBlog from "./Create";
+import BlogDetails from "./BlogDetails";
 
 function App() {
-  const title = "Welcome To New Genesis";
-  const likes = 50;
-  const link = 'http://www.google.com'
-  // const person ={ name:'yoshi',age:'50'};
+  
 
   return (
+    <Router>
     <div className="App">
     <Navbar/>
       <div className="content">
-        <Home/>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/createBlog' component={CreateBlog} />
+          <Route exact path='/blogs/:id' component={BlogDetails} />
+        </Switch>
       </div>
-      {/* <div className="nav active" id='nav'>
-          <a href="1"><i className="fa-game-console-handheld"></i><h4>PS4</h4></a>
-          <a href="1"><i className="fa fa-galaxy"></i><h4>XBOX</h4></a>
-          <a href="1"><i className="fa-game-console-handheld"></i><h4>PS5</h4></a>
-        </div>  */}
     </div>
+    </Router>
   );
 }
 
